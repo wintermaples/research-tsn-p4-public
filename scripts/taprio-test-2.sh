327fe5516ac52c75e9b4942a86e43259b39a5747
wintermaples <pg.wintermaples@gmail.com>
2023-09-27 22:38:26 +0900

    few

--- scripts/taprio-test-2.sh
+++ scripts/taprio-test-2.sh
@@ -8,18 +8,18 @@ num_tc 3 \
 map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 \
 queues 1@0 1@0 1@0 \
 base-time 0 \
-sched-entry S 01 30000 \
-sched-entry S 02 30000 \
-sched-entry S 04 720000 \
+sched-entry S 01 60000 \
+sched-entry S 02 120000 \
+sched-entry S 04 300000 \
 flags 0x1 \
 txtime-delay 40000 \
 clockid CLOCK_TAI
 
 tc qdisc replace dev eth0 parent 100:1 etf \
 skip_sock_check \
-delta 800000 clockid CLOCK_TAI
+delta 20000 clockid CLOCK_TAI
 
 tc qdisc replace dev eth0 parent 100:2 etf \
 skip_sock_check \
-delta 800000 clockid CLOCK_TAI
+delta 20000 clockid CLOCK_TAI
 
