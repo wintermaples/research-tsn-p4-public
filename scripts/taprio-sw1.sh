764fdbb0fa25a5e04a97ce814287886f2a733de0
wintermaples <pg.wintermaples@gmail.com>
2023-10-24 09:53:56 +0900

    aa

--- scripts/taprio-sw1.sh
+++ scripts/taprio-sw1.sh
@@ -10,6 +10,8 @@ ethtool -K enp3s0 tso off
 pkill -f phc2sys
 phc2sys -s /dev/ptp0 -c CLOCK_REALTIME -O 0 &
 
+
+##### enp1s0 #####
 tc qdisc del dev enp1s0 root
 
 tc qdisc replace dev enp1s0 parent root \
@@ -30,5 +32,30 @@ skip_sock_check \
 offload delta 200000 clockid CLOCK_TAI
 
 tc qdisc replace dev enp1s0 parent 100:2 etf \
+skip_sock_check \
+offload delta 200000 clockid CLOCK_TAI
+
+
+##### enp3s0
+tc qdisc del dev enp3s0 root
+
+tc qdisc replace dev enp3s0 parent root \
+handle 100 taprio \
+num_tc 3 \
+map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 \
+queues 1@0 1@0 1@0 \
+base-time 0 \
+sched-entry S 01 900000 \
+sched-entry S 02 900000 \
+sched-entry S 04 300000 \
+flags 0x1 \
+txtime-delay 40000 \
+clockid CLOCK_TAI
+
+tc qdisc replace dev enp3s0 parent 100:1 etf \
+skip_sock_check \
+offload delta 200000 clockid CLOCK_TAI
+
+tc qdisc replace dev enp3s0 parent 100:2 etf \
 skip_sock_check \
 offload delta 200000 clockid CLOCK_TAI
